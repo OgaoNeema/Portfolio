6d4ea6a7fb9827d30d06fa3b77a06262010d9c3e
OgaoNeema <neemaogao@gmail.com>
2025-04-03 01:42:46 +0300

    Worked on the navigation bar's basic content structure.

--- my-portfolio/src/Home.jsx
+++ my-portfolio/src/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Favicon from 'react-favicon' //For icons
 import './Home.css'
 import Nav from './Nav'
 
@@ -6,8 +7,34 @@ const Home = () => {
   return (
     <>
     <div className='Home-container'>
-      <Nav/>
+      <div className='Hero-Section'>
+        <Nav/>
+        <div className='Home-containter-left'>
+          <span className='Intro1'>Hello Word! I'm </span><span className='Intro2'>Neema Ogao</span>
+          <p className='Welcometext'>Welcome to my digital space.</p>
+          <div className='WelcomeBtns'>
+            <button className='Projects'>Projects</button>
+            <button className='contacts'>Contacts</button>
+          </div>
+        </div>
+        
+        <div className='Home-containter-right'>
+          right side animation
+        </div>
       </div>
+      <div className='AboutMe-Section'>
+        <div className='About-Title'>
+          <h1 className='AboutMe'>ABOUT ME</h1>
+          <h4 className='About-Description'>Transforming ideas into digital solutions.</h4>
+        </div>
+
+        <div className='About-Content'>
+          <p className='About-Description'> My name is Neema Ogao, and this is my personal portfolio; 
+            a garden where innovation and creativity grow. Here's a few things about me:</p>
+          
+        </div>
+      </div>
+    </div>
     </>
   )
 }
--- my-portfolio/src/Nav.jsx
+++ my-portfolio/src/Nav.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Nav.css'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='Navigation'>
+      <div className='Logo'>Logo</div>
+      <div className='Nav-links'>
+        <a href='' className='NavBtn'>Home</a>
+        <a href='' className='NavBtn'>About Me</a>
+        <a href='' className='NavBtn'>Portfolio</a>
+        <a href='' className='NavBtn'>Get in touch</a>
+      </div>
+    </div>
   )
 }
 
